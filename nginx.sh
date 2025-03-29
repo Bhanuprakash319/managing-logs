de7087180c1771b84cdfb351268855a09d4ecf78
Bhanuprakash319 <puthabhanu@gmail.com>
2025-03-29 10:04:46 +0530

    set-command

--- nginx.sh
+++ nginx.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 userid=$(id -u)
 if [ $userid -eq 0 ]
 then 
@@ -9,24 +11,24 @@ else
     exit 1
 fi
 
-validate()
-{
-    if [ $1 -eq 0 ]
-    then 
-         echo "$2 is successful"
-    else
-         echo "$2 is failure"
-         exit 1 
-    fi
-}
+# validate()
+# {
+#     if [ $1 -eq 0 ]
+#     then 
+#          echo "$2 is successful"
+#     else
+#          echo "$2 is failure"
+#          exit 1 
+#     fi
+# }
 
 
 dnf install nginx -y
-validate $? "installing nginx"
+#validate $? "installing nginx"
 
 systemctl enable nginx
-validate $? "enabling nginx"
+#validate $? "enabling nginx"
 
 systemctl start nginx
-validate $? "starting nginx"
+#validate $? "starting nginx"
 
